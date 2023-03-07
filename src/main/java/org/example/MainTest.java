f9a32858c0e892e6474b5abcb598c497dbe26f11
lukas <lukas.henke.berlin@gmx.de>
2023-03-07 15:37:41 +0100

    added junit4

---
+++ src/main/java/org/example/MainTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void main() {
+        assert(true);
+    }
+}
