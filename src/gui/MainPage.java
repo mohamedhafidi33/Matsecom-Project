e5d0a4c5a33ea67ab1917f4d38a914bceb6b9a7b
ll8987s <luisa.luettgens@alumni.fh-aachen.de>
2023-03-08 11:11:44 +0100

    added java swing form fix

--- src/gui/MainPage.java
+++ src/gui/MainPage.java
@@ -1,18 +1,17 @@
-package gui;
+package src.gui;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainPage {
-    private JButton b;
-    private JPanel panel1;
-    public MainPage() {
-        b.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+    private JButton button1;
+public MainPage() {
+    button1.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
 
-            }
-        });
-    }
+        }
+    });
+}
 }
