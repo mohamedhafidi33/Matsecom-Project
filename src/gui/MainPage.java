eec6226f0a7b0964250d42efc34a991c90d6a76d
ll8987s <luisa.luettgens@alumni.fh-aachen.de>
2023-03-08 11:06:05 +0100

    added java swing form

---
+++ src/gui/MainPage.java
@@ -0,0 +1,18 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainPage {
+    private JButton b;
+    private JPanel panel1;
+    public MainPage() {
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
