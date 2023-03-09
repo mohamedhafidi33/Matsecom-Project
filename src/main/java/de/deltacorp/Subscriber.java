c5e1a65ac25c41c1be2c42aef0880dd3f15d7ced
Yannick Gasper <yannick.gasper@devolo.de>
2023-03-09 12:25:08 +0100

    Add Getter and Setter for Subscriber

--- src/main/java/de/deltacorp/Subscriber.java
+++ src/main/java/de/deltacorp/Subscriber.java
@@ -25,5 +25,85 @@ public class Subscriber {
 		this.terminal = terminal;
 		this.subscription = subscription;
 	}
+
+	public double getRemainingDataVolumeMb() {
+		return this.remainingDataVolumeMb;
+	}
+
+	public void setRemainingDataVolumeMb(double remainingDataVolumeMb) {
+		this.remainingDataVolumeMb = remainingDataVolumeMb;
+	}
+
+	public double getRemainingFreeMinutes() {
+		return this.remainingFreeMinutes;
+	}
+
+	public void setRemainingFreeMinutes(double remainingFreeMinutes) {
+		this.remainingFreeMinutes = remainingFreeMinutes;
+	}
+
+	public double getChargingTotalEur() {
+		return this.chargingTotalEur;
+	}
+
+	public void setChargingTotalEur(double chargingTotalEur) {
+		this.chargingTotalEur = chargingTotalEur;
+	}
+
+	public String getForename() {
+		return this.forename;
+	}
+
+	public void setForename(String forename) {
+		this.forename = forename;
+	}
+
+	public String getSurname() {
+		return this.surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getMCC() {
+		return this.MCC;
+	}
+
+	public void setMCC(String MCC) {
+		this.MCC = MCC;
+	}
+
+	public String getMNC() {
+		return this.MNC;
+	}
+
+	public void setMNC(String MNC) {
+		this.MNC = MNC;
+	}
+
+	public String getMSIN() {
+		return this.MSIN;
+	}
+
+	public void setMSIN(String MSIN) {
+		this.MSIN = MSIN;
+	}
+
+	public Terminal getTerminal() {
+		return this.terminal;
+	}
+
+	public void setTerminal(Terminal terminal) {
+		this.terminal = terminal;
+	}
+
+	public Subscription getSubscription() {
+		return this.subscription;
+	}
+
+	public void setSubscription(Subscription subscription) {
+		this.subscription = subscription;
+	}
 	
 }
