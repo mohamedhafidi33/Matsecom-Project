f3c7a009165fbff7c6a39729aa7951692e4dddd7
Yannik Schreiter <Yannik.Schreiter@ericsson.com>
2023-03-09 19:27:15 +0100

    Gui should now work perfect

--- src/main/java/de/deltacorp/Gui.java
+++ src/main/java/de/deltacorp/Gui.java
@@ -13,7 +13,9 @@ public class Gui {
 
     public boolean isEditing = false;
     public static GuiViewModel guiViewModel = new GuiViewModel();
-    public static ArrayList<Subscriber> subscribers;
+    public static ArrayList<Subscriber> subscribers = new ArrayList<Subscriber>();
+    public static ArrayList<Session> sessions = new ArrayList<Session>();
+    public static ArrayList<Invoice> invoices = new ArrayList<Invoice>();
 
     private JPanel DesignPanel;
     private JButton managementOfSubscribersButton;
@@ -35,6 +37,29 @@ public class Gui {
     private JComboBox SubscriberListDropDown;
     private JComboBox InvoiceDropDownList;
     private JComboBox SessionDropDownList;
+    public void updateSessionDropDown(){
+        SessionDropDownList.removeAllItems();
+        if(sessions==null){
+            SessionDropDownList.setEnabled(false);
+        } else {
+            SessionDropDownList.setEnabled(true);
+            for(Session x: sessions){
+                SessionDropDownList.addItem(x.getSurname());
+            }
+        }
+    }
+
+    public void updateInvoiceDropDown(){
+        InvoiceDropDownList.removeAllItems();
+        if(invoices==null){
+            InvoiceDropDownList.setEnabled(false);
+        } else {
+            InvoiceDropDownList.setEnabled(true);
+            for (Invoice x: invoices){
+                InvoiceDropDownList.addItem(x.getSubscriberFullName());
+            }
+        }
+    }
 
     public void updateSubscriberDropDown(){
         ChooseSubscriberSession.removeAllItems();
@@ -48,9 +73,9 @@ public class Gui {
             SubscriberListDropDown.setEnabled(true);
             ChooseSubscriberSession.setEnabled(true);
             for(Subscriber x: subscribers){
-                ChooseSubscriberSession.addItem(x.forename);
-                ChooseEditSubscriber.addItem(x.forename);
-                SubscriberListDropDown.addItem(x.forename);
+                ChooseSubscriberSession.addItem(x.getMSIN());
+                ChooseEditSubscriber.addItem(x.getMSIN());
+                SubscriberListDropDown.addItem(x.getMSIN());
             }
         }
     }
@@ -59,6 +84,9 @@ public class Gui {
         JFormattedTextField tf = ((JSpinner.DefaultEditor) ChooseMinutesOfSession.getEditor()).getTextField();
         tf.setEditable(false);
         updateSubscriberDropDown();
+        updateSessionDropDown();
+        updateInvoiceDropDown();
+
         ChooseEditSubscriber.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
@@ -73,7 +101,7 @@ public class Gui {
                         IMSIText.setEditable(true);
                     } else {
                         for(Subscriber x: subscribers){
-                            if(x.forename==ChooseEditSubscriber.getSelectedItem().toString()){
+                            if(x.getMSIN()==ChooseEditSubscriber.getSelectedItem().toString()){
                                 ForenameText.setText(x.forename);
                                 SurnameText.setText(x.surname);
                                 IMSIText.setText(x.getMSIN());
@@ -156,20 +184,80 @@ public class Gui {
         SubscriberListDropDown.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
-                DefaultListModel listModel = new DefaultListModel<>();
-                listModel.add(0, "Info about " + SubscriberListDropDown.getSelectedItem());
-                for(Subscriber x : subscribers){
-                    if(x.forename==SubscriberListDropDown.getSelectedItem()){
-                        listModel.add(1, x.terminal.name);
-                        listModel.add(2,x.subscription.name);
+                if(subscribers!=null){
+                    DefaultListModel listModel = new DefaultListModel<>();
+                    listModel.add(0, "Info about " + SubscriberListDropDown.getSelectedItem());
+                    for(Subscriber x : subscribers){
+                        if(x.getMSIN()==SubscriberListDropDown.getSelectedItem()){
+                            listModel.add(1, x.terminal.name);
+                            listModel.add(2,x.subscription.name);
+                        }
+                    }
+                    list1.setModel(listModel);
+                }
+            }
+        });
+        addSessionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(Integer.parseInt(ChooseMinutesOfSession.getValue().toString())==0){
+                    JOptionPane.showMessageDialog(null,"You should increase the time.");
+                } else if (ChooseSubscriberSession.getSelectedItem()==null) {
+                    JOptionPane.showMessageDialog(null, "You should firt register a Subscriber.");
+                } else {
+                    for (Subscriber x : subscribers) {
+                        if (x.getMSIN() == ChooseSubscriberSession.getSelectedItem()) {
+                            try {
+                                sessions = guiViewModel.addSession(x.forename, x.surname, ChooseMinutesOfSession.getValue().toString(), ChooseServiceSession.getSelectedItem().toString());
+                            } catch (IllegalArgumentException illegalArgumentException) {
+                                JOptionPane.showMessageDialog(null, illegalArgumentException.getMessage());
+                            }
+                        }
+                    }
+                    updateSessionDropDown();
+                }
+            }
+        });
+        SessionDropDownList.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                for(Session x: sessions){
+                    if(x.getSurname()==SessionDropDownList.getSelectedItem()){
+                        DefaultListModel listModel = new DefaultListModel<>();
+                        listModel.add(0,"Sessioninformation from " + x.getSurname());
+                        listModel.addElement(x.getService());
+                        listModel.addElement(x.getDurationSeconds());
                     }
                 }
-                list1.setModel(listModel);
+            }
+        });
+        callInvoiceButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                invoices = guiViewModel.addInvoice();
+                updateInvoiceDropDown();
+            }
+        });
+        InvoiceDropDownList.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                for(Invoice x: invoices){
+                    if(x.getSubscriberFullName()==SessionDropDownList.getSelectedItem()){
+                        DefaultListModel listModel = new DefaultListModel<>();
+                        listModel.add(0,"Sessioninformation from " + x.getSubscriberFullName());
+                        listModel.add(1,"Used Minutes:" + x.getUsedMinutes());
+                        listModel.add(2,"Left Charges:" + x.getAppliedChargesEur() + "€");
+                        listModel.add(3,"Used Data:" + x.getUsedDataMb() + "Mb");
+                    }
+                }
             }
         });
     }
 
     public static void main(String[] args) {
+        subscribers = guiViewModel.getSubscribers();
+        sessions = guiViewModel.getSessions();
+        invoices = guiViewModel.getInvoices();
         frame = new JFrame("GUI");
         instance = new Gui();
 
@@ -177,7 +265,6 @@ public class Gui {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        subscribers = guiViewModel.getSubscribers();
 
     }
 
