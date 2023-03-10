9ef7db7cc6d92506a6d60b23c8021cfc2b9125db
Yannick Gasper <yannick.gasper@devolo.de>
2023-03-10 12:16:43 +0100

    aoifn

--- src/main/java/de/deltacorp/GuiViewModel.java
+++ src/main/java/de/deltacorp/GuiViewModel.java
@@ -142,6 +142,7 @@ public class GuiViewModel {
             Invoice invoice = new Invoice(mCC + mNC + sub.getMSIN(), sub.forename + " " + sub.surname, sub.usedDataVolume, sub.usedMinutes, sub.chargingTotalEur);
             sub.usedDataVolume = 0;
             sub.usedDataVolume = 0;
+            sub.usedMinutes = 0;
             sub.chargingTotalEur = sub.subscription.basicfee;
             sub.remainingFreeMinutes = sub.subscription.minutesIncluded;
             sub.remainingDataVolumeMb = sub.subscription.dataVolumeMB;
